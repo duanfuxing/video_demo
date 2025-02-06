f1135236743741225bb8853e48a27587234ebdb6
duanfuxing <duanfuxing@taido.com.cn>
2025-02-06 16:09:14 +0800

    1111

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ def download_video(filename):
     return send_file(video_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
